c1924ea72dd1cee938e7ab1f14df57eb3b3a4b13
Fabio Lucas <fabiolucamz@gmail.com>
2026-02-19 15:33:39 -0300

    first commit

---
+++ src/clinica/area_paciente/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'area_paciente/index.html')
+
+def dashboard(request):
+    return render(request, 'area_paciente/dashboard.html')
