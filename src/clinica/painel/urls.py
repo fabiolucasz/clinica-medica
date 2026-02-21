d0f7a6d27138ab6b134f1d77e109bf8f7e09c7a3
Fabio Lucas <fabiolucamz@gmail.com>
2026-02-21 15:02:55 -0300

    feat: adicionado listar pacientes

--- src/clinica/painel/urls.py
+++ src/clinica/painel/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("painel/", views.index, name="painel"),
     path("cadastrar-paciente/", views.cadastrar_paciente, name="cadastrar_paciente"),
+    path("listar-pacientes/", views.listar_pacientes, name="listar_pacientes"),
+    path("editar-paciente/<int:id>/", views.editar_paciente, name="editar_paciente"),
+    path("excluir-paciente/<int:id>/", views.excluir_paciente, name="excluir_paciente"),
     path('logout/', views.logout_view, name='logout'),
 ]
