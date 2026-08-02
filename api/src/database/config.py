67c1a3fb8c07c4c9a6b44ab3880d190b1ec78c6c
Fabio Lucas <fabiolucamz@gmail.com>
2026-08-02 19:48:09 -0300

    fix: alterado constante de conexão com banco de dados de DBNAME para DATABASE_NAME por exigência do validador pydantic

--- api/src/database/config.py
+++ api/src/database/config.py
@@ -1,52 +1,104 @@
 import os
 
+
+
 from pydantic import PostgresDsn, computed_field
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+
 env_path = os.path.join(
+
     os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env"
+
 )
+
+
 
 print(env_path)
 
 
+
+
+
 class Settings(BaseSettings):
+
     model_config = SettingsConfigDict(
+
         env_file=env_path,
+
         env_file_encoding="utf-8",
+
         env_ignore_empty=True,
+
         extra="ignore",
+
     )
 
+
+
     ENVIRONMENT: str = "dev"
+
     DOMAIN: str = "localhost"
+
     DATABASE_URL: str = "sqlite:///./clinica.db"
+
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     SECRET_KEY: str
+
     ALGORITHM: str
 
+
+
     # PostgreSQL for production (optional for dev)
+
     SCHEME: str = "postgresql+psycopg2"
+
     HOST: str
+
     USER: str
+
     PORT: int
-    DBNAME: str
+
+    DATABASE_NAME: str
+
     PASSWORD: str
 
-    @computed_field
-    @property
-    def server_host(self) -> str:
-        if self.ENVIRONMENT == "dev":
-            return f"http://{self.DOMAIN}"
-        return f"https://{self.DOMAIN}"
+
 
     @computed_field
+
     @property
-    def SQLALCHEMY_DATABASE_URI(self) -> str | PostgresDsn:
+
+    def server_host(self) -> str:
+
         if self.ENVIRONMENT == "dev":
+
+            return f"http://{self.DOMAIN}"
+
+        return f"https://{self.DOMAIN}"
+
+
+
+    @computed_field
+
+    @property
+
+    def SQLALCHEMY_DATABASE_URI(self) -> str | PostgresDsn:
+
+        if self.ENVIRONMENT == "dev":
+
             return self.DATABASE_URL
+
         else:
-            return f"{self.SCHEME}://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DBNAME}"
+
+            return f"{self.SCHEME}://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE_NAME}"
+
+
+
 
 
 settings = Settings()  # type: ignore
+
